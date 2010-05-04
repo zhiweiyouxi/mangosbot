9a57a1285210e6c1119ec9cc73484b38ee497620
Sergey Kuzmin <kuzminsv@gmail.com>
2010-05-04 11:49:46 +0400

    test improvements

--- src/tests/MageNonCombatTestCase.cpp
+++ src/tests/MageNonCombatTestCase.cpp
@@ -1,57 +1,34 @@
 #include "pch.h"
 
-#include "../game/Action.h"
-#include "../game/ActionBasket.h"
-#include "../game/Queue.h"
-#include "../game/Trigger.h"
-#include "../game/Engine.h"
+#include "EngineTestBase.h"
 #include "../game/MageActionFactory.h"
 #include "../game/GenericMageNonCombatStrategy.h"
-
-#include "MockPlayerbotAIFacade.h"
 
 using namespace ai;
 
 
-class MageNonCombatTestCase : public CPPUNIT_NS::TestFixture
+class MageNonCombatTestCase : public EngineTestBase
 {
     CPPUNIT_TEST_SUITE( MageNonCombatTestCase );
     CPPUNIT_TEST( buff );
     CPPUNIT_TEST_SUITE_END();
 
-protected:
-    MockPlayerbotAIFacade *ai;
-    Engine *engine;
-
 public:
     void setUp()
     {
-        ai = new MockPlayerbotAIFacade();
-
-        engine = new Engine(ai, new MageActionFactory(ai));
-        engine->addStrategy("nc");
-        engine->Init();
-    }
-
-    void tearDown()
-    {
-        if (engine)
-            delete engine;
-        if (ai) 
-            delete ai;
+		EngineTestBase::setUp();
+		setupEngine(new MageActionFactory(ai), "nc", NULL);
     }
 
 protected:
     void buff()
     {
-        engine->DoNextAction(NULL);
-        ai->auras.push_back("arcane intellect");
-        ai->spellCooldowns.remove("arcane intellect");
+		tick();
+        addAura("arcane intellect");
 
-        engine->DoNextAction(NULL);
+		tickWithSpellAvailable("arcane intellect");
 
-        std::cout << ai->buffer;
-        CPPUNIT_ASSERT(!strcmp(ai->buffer.c_str(), ">arcane intellect>arcane intellect on party"));
+        assertActions(">arcane intellect>arcane intellect on party");
     }
     
 };
