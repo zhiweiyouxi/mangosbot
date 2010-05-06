7dde53d95ce42f036f0f81e4746d1f110b65da88
Sergey Kuzmin <kuzminsv@gmail.com>
2010-05-06 12:16:13 +0400

    find items is case insensitive

--- src/game/PlayerbotAIFacade.cpp
+++ src/game/PlayerbotAIFacade.cpp
@@ -166,10 +166,40 @@ bool PlayerbotAIFacade::isDrink(const ItemPrototype* pItemProto, const void* par
         pItemProto->Spells[0].SpellCategory == 59);
 }
 
+
+char * __cdecl strstri (
+					   const char * str1,
+					   const char * str2
+					   )
+{
+	char *cp = (char *) str1;
+	char *s1, *s2;
+
+	if ( !*str2 )
+		return((char *)str1);
+
+	while (*cp)
+	{
+		s1 = cp;
+		s2 = (char *) str2;
+
+		while ( *s1 && *s2 && !(tolower(*s1)-tolower(*s2)) )
+			s1++, s2++;
+
+		if (!*s2)
+			return(cp);
+
+		cp++;
+	}
+
+	return(NULL);
+
+}
+
 bool PlayerbotAIFacade::isTheSameName(const ItemPrototype* pItemProto, const void* param)
 {
 	const char* name = (const char*)param;
-	return pItemProto && pItemProto->Name1 && !strcmpi(pItemProto->Name1, name);
+	return pItemProto && pItemProto->Name1 && strstri(pItemProto->Name1, name);
 }
 
 Player* PlayerbotAIFacade::GetPartyMinHealthPlayer()
