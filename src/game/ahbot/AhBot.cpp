a586f14c7d67bdbfee50eaba59fa39daedf5fc5c
Ike Erhascy <ike3@aloe.no-ip.biz>
2012-12-24 12:01:21 +0400

    [ai-43] possible crash fix

--- src/game/ahbot/AhBot.cpp
+++ src/game/ahbot/AhBot.cpp
@@ -219,17 +219,17 @@ void AhBot::Answer(int auction, Category* category, ItemBag* inAuctionItems)
 
         if (entry->buyout && (entry->bid >= entry->buyout || 100 * (entry->buyout - entry->bid) / price < 25))
         {
+            sLog.outDetail("AhBot %d won %s in auction %d for %d", bidder, item->GetProto()->Name1, auctionIds[auction], entry->buyout);
+
             entry->bid = entry->buyout;
             entry->AuctionBidWinning(NULL);
-
-            sLog.outDetail("AhBot %d won %s in auction %d for %d", bidder, item->GetProto()->Name1, auctionIds[auction], entry->buyout);
         }
         else
         {
+            sLog.outDetail("AhBot %d placed bid %d for %s in auction %d", bidder, entry->bid, item->GetProto()->Name1, auctionIds[auction]);
+
             CharacterDatabase.PExecute("UPDATE auction SET buyguid = '%u',lastbid = '%u' WHERE id = '%u'",
                 entry->bidder, entry->bid, entry->Id);
-
-            sLog.outDetail("AhBot %d placed bid %d for %s in auction %d", bidder, entry->bid, item->GetProto()->Name1, auctionIds[auction]);
         }
    }
 }
