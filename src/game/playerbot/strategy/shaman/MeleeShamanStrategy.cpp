1fb3e98c81509f011ab98cbd9ec53b7db5ea6c89
ike3 <ike3@aloe.no-ip.biz>
2013-01-08 08:26:13 +0400

    [ai-112] melee shaman: not facing target and enemy too close for melee triggers moved to non-aoe strategy

--- src/game/playerbot/strategy/shaman/MeleeShamanStrategy.cpp
+++ src/game/playerbot/strategy/shaman/MeleeShamanStrategy.cpp
@@ -63,21 +63,22 @@ void MeleeShamanStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
     triggers.push_back(new TriggerNode(
         "shock",
         NextAction::array(0, new NextAction("flame shock", 20.0f), NULL)));
-}
 
-void MeleeAoeShamanStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
-{
     triggers.push_back(new TriggerNode(
         "not facing target",
         NextAction::array(0, new NextAction("set facing", ACTION_NORMAL + 7), NULL)));
 
     triggers.push_back(new TriggerNode(
-        "enemy out of melee",
-        NextAction::array(0, new NextAction("reach melee", ACTION_NORMAL + 8), NULL)));
-
-    triggers.push_back(new TriggerNode(
         "enemy too close for melee",
         NextAction::array(0, new NextAction("move out of enemy contact", ACTION_NORMAL + 8), NULL)));
+
+}
+
+void MeleeAoeShamanStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
+{
+    triggers.push_back(new TriggerNode(
+        "enemy out of melee",
+        NextAction::array(0, new NextAction("reach melee", ACTION_NORMAL + 8), NULL)));
 
     triggers.push_back(new TriggerNode(
         "magma totem",
