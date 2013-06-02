f1701d5852ceaf2b66c1bb91b41e6945c32b01d6
ike3 <ike3@aloe.no-ip.biz>
2013-06-02 08:36:20 +0400

    [ai-0.31-22] /1 who [item] command to query nearby bots who can sell this item

--- src/game/playerbot/strategy/actions/WhoAction.cpp
+++ src/game/playerbot/strategy/actions/WhoAction.cpp
@@ -3,6 +3,8 @@
 #include "WhoAction.h"
 #include "../../AiFactory.h"
 #include "../ItemVisitors.h"
+#include "../../../ahbot/AhBot.h"
+#include "../../RandomPlayerbotMgr.h"
 
 using namespace ai;
 
@@ -15,106 +17,126 @@ bool WhoAction::Execute(Event event)
         return false;
 
     ostringstream out;
-
-    int spec = AiFactory::GetPlayerSpecTab(bot);
-    out << "|h|cffffffff" << chat->formatClass(bot, spec);
-    out << "|h|cff00ff00 (" << bot->getLevel() << "|h|cffffffff lvl), ";
-
-    ItemCountByQuality visitor;
-    IterateItems(&visitor, ITERATE_ITEMS_IN_EQUIP);
-
-    bool needSlash = false;
-    if (visitor.count[ITEM_QUALITY_EPIC])
+    string text = event.getParam();
+    if (!text.empty())
     {
-        out << "|h|cffff00ff" << visitor.count[ITEM_QUALITY_EPIC] << "|h|cffffffff";
-        needSlash = true;
-    }
+        if (!sRandomPlayerbotMgr.IsRandomBot(bot))
+            return false;
 
-    if (visitor.count[ITEM_QUALITY_RARE])
-    {
-        if (needSlash) out << "/";
-        out << "|h|cff8080ff" << visitor.count[ITEM_QUALITY_RARE] << "|h|cffffffff";
-        needSlash = true;
-    }
+        list<Item*> items = InventoryAction::parseItems(text);
+        if (items.empty())
+            return false;
 
-    if (visitor.count[ITEM_QUALITY_UNCOMMON])
-    {
-        if (needSlash) out << "/";
-        out << "|h|cff00ff00" << visitor.count[ITEM_QUALITY_UNCOMMON] << "|h|cffffffff";
-        needSlash = true;
-    }
-
-    if (needSlash)
-    {
-        out << " gear (|h|cff00ff00" << bot->GetEquipGearScore(false, false) << "|h|cffffffff GS)";
-    }
-
-    if (skills.empty())
-    {
-        skills[SKILL_ALCHEMY] = "Alchemy";
-        skills[SKILL_ENCHANTING] = "Enchanting";
-        skills[SKILL_SKINNING] = "Skinning";
-        skills[SKILL_JEWELCRAFTING] = "Jewelcrafting";
-        skills[SKILL_INSCRIPTION] = "Inscription";
-        skills[SKILL_TAILORING] = "Tailoring";
-        skills[SKILL_LEATHERWORKING] = "Leatherworking";
-        skills[SKILL_ENGINEERING] = "Engineering";
-        skills[SKILL_HERBALISM] = "Herbalism";
-        skills[SKILL_MINING] = "Mining";
-        skills[SKILL_BLACKSMITHING] = "Blacksmithing";
-        skills[SKILL_COOKING] = "Cooking";
-        skills[SKILL_FIRST_AID] = "First Aid";
-        skills[SKILL_FISHING] = "Fishing";
-    }
-
-    ObjectGuid guid = bot->GetObjectGuid();
-    for (map<uint32, string>::iterator i = skills.begin(); i != skills.end(); ++i)
-    {
-        uint16 skill = i->first;
-        if (!bot->HasSkill(skill))
-            continue;
-
-        string skillName = i->second;
-        uint32 spellId = AI_VALUE2(uint32, "spell id", skillName);
-        uint16 value = bot->GetSkillValue(skill);
-        uint16 maxSkill = bot->GetMaxSkillValue(skill);
-		string data = "0";
-        out << " |cFFFFFF00|Htrade:" << spellId << ":" << value << ":" << maxSkill << ":"
-                << std::hex << std::uppercase << guid.GetRawValue()
-                << std::nouppercase << std::dec << ":" << data
-                << "|h[" << skills[skill] << "]|h|r"
-                << " |h|cff00ff00" << value << "|h|cffffffff/"
-                << "|h|cff00ff00" << maxSkill << "|h|cffffffff ";
-    }
-
-    Player* master = GetMaster();
-    Group* group = master ? master->GetGroup() : bot->GetGroup();
-    if (group)
-    {
-        out << " in " << (int)group->GetMembersCount() <<  "-party with (";
-        int index = 1, followIndex = -1;
-        for (GroupReference *ref = group->GetFirstMember(); ref; ref = ref->next())
+        for (list<Item*>::iterator i = items.begin(); i != items.end(); ++i)
         {
-            Player* player = ref->getSource();
-            if (!player)
-                out << "?";
-            else if (player == master)
-                out << "!" << player->GetName() << "!";
-            else
-                out << player->GetName();
+            Item* sell = *i;
+            int32 sellPrice = auctionbot.GetSellPrice(sell->GetProto()) * sRandomPlayerbotMgr.GetSellMultiplier(bot);
+            out << "Selling " << chat->formatItem(sell->GetProto(), sell->GetCount()) << " for " << chat->formatMoney(sellPrice);
+            break;
+        }
+    }
+    else
+    {
+        int spec = AiFactory::GetPlayerSpecTab(bot);
+        out << "|h|cffffffff" << chat->formatClass(bot, spec);
+        out << "|h|cff00ff00 (" << bot->getLevel() << "|h|cffffffff lvl), ";
 
-            if (ref->next())
-                out << ", ";
+        ItemCountByQuality visitor;
+        IterateItems(&visitor, ITERATE_ITEMS_IN_EQUIP);
 
-            if(ref->getSource() == master)
+        bool needSlash = false;
+        if (visitor.count[ITEM_QUALITY_EPIC])
+        {
+            out << "|h|cffff00ff" << visitor.count[ITEM_QUALITY_EPIC] << "|h|cffffffff";
+            needSlash = true;
+        }
+
+        if (visitor.count[ITEM_QUALITY_RARE])
+        {
+            if (needSlash) out << "/";
+            out << "|h|cff8080ff" << visitor.count[ITEM_QUALITY_RARE] << "|h|cffffffff";
+            needSlash = true;
+        }
+
+        if (visitor.count[ITEM_QUALITY_UNCOMMON])
+        {
+            if (needSlash) out << "/";
+            out << "|h|cff00ff00" << visitor.count[ITEM_QUALITY_UNCOMMON] << "|h|cffffffff";
+            needSlash = true;
+        }
+
+        if (needSlash)
+        {
+            out << " gear (|h|cff00ff00" << bot->GetEquipGearScore(false, false) << "|h|cffffffff GS)";
+        }
+
+        if (skills.empty())
+        {
+            skills[SKILL_ALCHEMY] = "Alchemy";
+            skills[SKILL_ENCHANTING] = "Enchanting";
+            skills[SKILL_SKINNING] = "Skinning";
+            skills[SKILL_JEWELCRAFTING] = "Jewelcrafting";
+            skills[SKILL_INSCRIPTION] = "Inscription";
+            skills[SKILL_TAILORING] = "Tailoring";
+            skills[SKILL_LEATHERWORKING] = "Leatherworking";
+            skills[SKILL_ENGINEERING] = "Engineering";
+            skills[SKILL_HERBALISM] = "Herbalism";
+            skills[SKILL_MINING] = "Mining";
+            skills[SKILL_BLACKSMITHING] = "Blacksmithing";
+            skills[SKILL_COOKING] = "Cooking";
+            skills[SKILL_FIRST_AID] = "First Aid";
+            skills[SKILL_FISHING] = "Fishing";
+        }
+
+        ObjectGuid guid = bot->GetObjectGuid();
+        for (map<uint32, string>::iterator i = skills.begin(); i != skills.end(); ++i)
+        {
+            uint16 skill = i->first;
+            if (!bot->HasSkill(skill))
                 continue;
 
-            if(ref->getSource() == bot)
-                followIndex = index;
-
-            index++;
+            string skillName = i->second;
+            uint32 spellId = AI_VALUE2(uint32, "spell id", skillName);
+            uint16 value = bot->GetSkillValue(skill);
+            uint16 maxSkill = bot->GetMaxSkillValue(skill);
+            string data = "0";
+            out << " |cFFFFFF00|Htrade:" << spellId << ":" << value << ":" << maxSkill << ":"
+                    << std::hex << std::uppercase << guid.GetRawValue()
+                    << std::nouppercase << std::dec << ":" << data
+                    << "|h[" << skills[skill] << "]|h|r"
+                    << " |h|cff00ff00" << value << "|h|cffffffff/"
+                    << "|h|cff00ff00" << maxSkill << "|h|cffffffff ";
         }
-        out << ") " << followIndex << "th";
+
+        Player* master = GetMaster();
+        Group* group = master ? master->GetGroup() : bot->GetGroup();
+        if (group)
+        {
+            out << " in " << (int)group->GetMembersCount() <<  "-party with (";
+            int index = 1, followIndex = -1;
+            for (GroupReference *ref = group->GetFirstMember(); ref; ref = ref->next())
+            {
+                Player* player = ref->getSource();
+                if (!player)
+                    out << "?";
+                else if (player == master)
+                    out << "!" << player->GetName() << "!";
+                else
+                    out << player->GetName();
+
+                if (ref->next())
+                    out << ", ";
+
+                if(ref->getSource() == master)
+                    continue;
+
+                if(ref->getSource() == bot)
+                    followIndex = index;
+
+                index++;
+            }
+            out << ") " << followIndex << "th";
+        }
     }
 
     // ignore random bot chat filter
