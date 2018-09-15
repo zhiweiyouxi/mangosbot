94639ca15be21eb6a21b67e5dbf5edd2a8c2f47b
ike3 <ike@email.org>
2018-09-15 18:17:07 +0300

    [PLAYERBOTS] Performance optimization

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -616,6 +616,7 @@ class ChatHandler
         bool HandleRandomPlayerbotCommand(char* args);
         bool HandleAhBotCommand(char* args);
         bool HandleGuildTaskCommand(char* args);
+        bool HandlePerfMonCommand(char* args);
 #endif
 
         bool HandleLinkAddCommand(char* args);
