f0cf6b7901304383c0f124eaef40069bb3e4e23c
ike3 <ike3@email.org>
2018-09-28 11:52:27 +0300

    [IMMERSIVE] Immersive.FishingBaubles parameter to require baubles to fish (nothing will be caught without them)

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -41,6 +41,9 @@
 #include "vmap/GameObjectModel.h"
 #include "Server/SQLStorages.h"
 #include "World/WorldState.h"
+#ifdef ENABLE_IMMERSIVE
+#include "immersive.h"
+#endif
 
 GameObject::GameObject() : WorldObject(),
     m_model(nullptr),
@@ -352,7 +355,7 @@ void GameObject::Update(uint32 update_diff, uint32 p_time)
                             {
                                 if (m_respawnTime > 0)
                                     valid = false;
-                                else // battlegrounds gameobjects has data2 == 0 && data5 == 3                                
+                                else // battlegrounds gameobjects has data2 == 0 && data5 == 3
                                     radius = float(goInfo->trap.cooldown);
                             }
                         }
@@ -1461,6 +1464,9 @@ void GameObject::Use(Unit* user)
 
                     // normal chance
                     bool success = skill >= zone_skill && chance >= roll;
+#ifdef ENABLE_IMMERSIVE
+                    success = sImmersive.OnFishing(player, success);
+#endif
                     GameObject* fishingHole = nullptr;
 
                     // overwrite fail in case fishhole if allowed (after 3.3.0)
@@ -1672,7 +1678,7 @@ void GameObject::Use(Unit* user)
                 return;
 
             Player* player = (Player*)user;
-            
+
             delete loot;
             loot = new Loot(player, this, LOOT_FISHINGHOLE);
             loot->ShowContentTo(player);
