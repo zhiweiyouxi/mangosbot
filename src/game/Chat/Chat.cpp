608028dfe13427eea7f428c455bfb09436b36862
conan513 <conan_513@hotmail.com>
2019-06-16 10:14:04 +0200

    Add CmangosDB RU support

--- src/game/Chat/Chat.cpp
+++ src/game/Chat/Chat.cpp
@@ -1833,7 +1833,7 @@ bool ChatHandler::isValidChatMessage(const char* message) const
                                 return false;
                             }
 
-                            for (uint8 i = 0; i < MAX_LOCALE; ++i)
+                            for (uint8 i = 0; i < MAX_DBC_LOCALE; ++i)
                             {
                                 uint32 skillLineNameLength = strlen(skillLine->name[i]);
                                 if (skillLineNameLength > 0 && strncmp(skillLine->name[i], buffer, skillLineNameLength) == 0)
@@ -1846,7 +1846,7 @@ bool ChatHandler::isValidChatMessage(const char* message) const
                             }
                         }
                         bool foundName = false;
-                        for (uint8 i = 0; i < MAX_LOCALE; ++i)
+                        for (uint8 i = 0; i < MAX_DBC_LOCALE; ++i)
                         {
                             if (*linkedSpell->SpellName[i] && strcmp(linkedSpell->SpellName[i], buffer) == 0)
                             {
